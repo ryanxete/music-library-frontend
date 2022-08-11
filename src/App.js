fe5d00ddf292d94209d7608f890181aa6d79f066
Ryan Etedali <ryanetedali@gmail.com>
2022-08-10 20:21:47 -0500

    created fetchSongs in app.js using useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import SearchBar from './Components/SearchBar/SearchBar';
 import MusicTable from './Components/MusicTable/MusicTable';
+import axios from 'axios';
 
 function App() {
 
 const [library, setLibrary] = useState([{"artist": "Just Emma","name": "Passengers","album": "Scars In The Sky","genre": "House","release_date": "2016-03-09"
 }])
+
+useEffect(()=> {
+  fetchSongs();
+}, [])
+
+const fetchSongs = async () => {
+  try {
+    let respose = await axios.get("http://127.0.0.1:8000/api/music_library")
+    setLibrary(respose.data);
+    console.log("Songs: ", respose.data)
+  } catch (error) {
+    console.log("error in fetchSongs: ", error.respose.data);
+  }
+};
 
   return (
     <div >
